54054a7a326d8b47dacdf9de34cdee88211d60a2
Alexandr Pavlov <mr.alexdemonp2003@mail.ru>
2024-03-24 15:47:56 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {restaurants} from "./constants/mock";
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement)
+
+
+root.render(
+    <button id="myButton" onClick={() =>{
+        console.log("Hello!");
+    }}>
+        <span>Click</span>
+        <span>{restaurants[0].name}</span>
+    </button>
+);
